89bf8960d6dec5f4268260f5c17b8d23650b2655
williamdekou <williamdekou@gmail.com>
2017-04-14 10:34:44 +0100

    initial release

---
+++ index.js
@@ -0,0 +1,16 @@
+exports.printMsg = function() {
+  console.log("This is a message from the demo package");
+}
+
+
+'use strict';
+
+/**
+ * Adds commas to a number
+ * @param {number} number
+ * @param {string} locale
+ * @return {string}
+ */
+module.exports = function(number, locale) {
+    return number.toLocaleString(locale);
+};
